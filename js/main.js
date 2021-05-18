e6d3f417c662b58eacb753b9ff2994c9f3e341e1
Josh Dorn <jr_dorn@yahoo.com>
2021-05-17 21:51:32 -0500

    Initial list

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//select everything
+//select todo-form
+const todoForm = document.querySelector(".todo-form");
+//select input box
+const todoInput = document.querySelector(".todo-input");
+//select ul with class .todo-items
+const todoItemsList = document.querySelector(".todo-items");
+//array to store every todo
+const todos = [];
